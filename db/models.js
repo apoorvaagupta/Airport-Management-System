54f1c62d9f2f5d5809abea29af0d00b40fcbfa0c
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-08 22:05:21 +0530

    added the database initial commit
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ db/models.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+// const DATABASE_URL = ('postgres://' + secret.DB_USER + ":" + secret.DB_PASSWORD + "@" + secret.DB_HOST + ":5432/" + secret.DATABASE);
+
+
+const db = new Sequelize(
+  'airportdb',
+  'airportuser',
+  'airportpass',
+  {
+    dialect: 'postgres',
+    host: 'localhost'
+  });
+
+db.sync().then(() => console.log("Database Ready"))
+
+module.exports = {
+  db: db
+};
