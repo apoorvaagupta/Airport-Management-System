93e1e7cb757e4a5e2320314c3c6e8ecb6ec5c012
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-12 00:35:48 +0530

    added the route for airport
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ routes/airport.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const route = express.Router();
+const db = require('../db/models');
+
+route.post('/add', function (req, res) {
+
+  db.query(`INSERT INTO applicant values(${req.body.airport_code},'${req.body.name}','${req.body.city}','${req.body.country}',${req.body.contact_no},'${req.body.email}')`).then((data) => {
+    console.log(data)
+    res.send("Airport added");
+  }).catch((err) => {
+    console.log(err);
+    res.send(err);
+  })
+})
+
+
+route.get('/viewAll', (req, res) => {
+  db.query("SELECT * FROM airport;").then((airports) => {
+    console.log(airports)
+    res.send(airports[0])
+  })
+})
+
+
+module.exports = route;
