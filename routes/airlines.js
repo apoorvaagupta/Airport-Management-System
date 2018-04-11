2aea0fc0f4f3e6fc13572af1651c2d1ec419694f
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-12 00:35:38 +0530

    added the route for airlines
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ routes/airlines.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const route = express.Router();
+const db = require('../db/models');
+
+route.post('/add', function (req, res) {
+
+  db.query(`INSERT INTO airlines values(${req.body.airline_code},'${req.body.name}')`).then((data) => {
+    console.log(data)
+    res.send("Airlines added");
+  }).catch((err) => {
+    console.log(err);
+    res.send(err);
+  })
+})
+
+
+route.get('/viewAll', (req, res) => {
+  db.query("SELECT * FROM airlines;").then((airlines) => {
+    console.log(airlines)
+    res.send(airlines[0])
+  })
+})
+
+
+module.exports = route;
