99d7e50b100c320b4f6ab3a5ad6cd33a0948a212
tld3112 <tkgs.aditi.tayal@gmail.com>
2018-04-12 08:27:30 +0530

    added related db functions

--- db/queries/index.js
+++ db/queries/index.js
@@ -1,5 +1,14 @@
 
 module.exports = {
-  airport: require('./airport'),
-
+    airport: require('./airport'),
+    airlines: require('./airlines'),
+    airplanes: require('./airplanes'),
+    baggage_counters: require('./baggage_counters'),
+    check_in_rows: require('./check_in_rows'),
+    flights: require('./flights'),
+    gates: require('./gates'),
+    landing_flights: require('./landing_flights'),
+    takeoff_flights: require('./takeoff_flights'),
+    terminals: require('./terminals'),
+    runways: require('./runways')
 }
--- db/utils/createAllTables.js
+++ db/utils/createAllTables.js
@@ -5,3 +5,44 @@ const queries = require('../queries');
 db.query(queries.airport.createTable).then((data) => {
 
 });
+
+db.query(queries.airlines.createTable).then((data) => {
+
+});
+
+db.query(queries.airplanes.createTable).then((data) => {
+
+});
+
+db.query(queries.baggage_counters.createTable).then((data) => {
+
+});
+
+db.query(queries.check_in_rows.createTable).then((data) => {
+
+});
+
+db.query(queries.flights.createTable).then((data) => {
+
+});
+
+db.query(queries.takeoff_flights.createTable).then((data) => {
+
+});
+
+db.query(queries.landing_flights.createTable).then((data) => {
+
+});
+
+db.query(queries.runways.createTable).then((data) => {
+
+});
+
+db.query(queries.takeoff_flights.createTable).then((data) => {
+
+});
+
+db.query(queries.terminals.createTable).then((data) => {
+
+});
+
