d9bfa1be758d990d72d367ffb96a6e2d530b47c3
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-17 00:06:51 +0530

    added the route for terminals
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ routes/terminals.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const route = express.Router();
+const db = require('../db/models');
+
+route.post('/add', function (req, res) {
+
+  db.query(`INSERT INTO terminals values(${req.body.terminal_id},'${req.body.name}')`).then((data) => {
+    console.log(data)
+    res.send("Terminal added");
+  }).catch((err) => {
+    console.log(err);
+    res.send(err);
+  })
+})
+
+
+route.get('/viewAll', (req, res) => {
+  db.query("SELECT * FROM terminals;").then((terminals) => {
+    console.log(terminals)
+    res.send(terminals[0])
+  })
+})
+
+
+module.exports = route;
