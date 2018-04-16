9a1332156c5a5dedf282197f90c3d46fe123cdb3
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-17 01:07:53 +0530

    added the mappings
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const route = require('express').Router()
 
+route.use('/airplanes', require('./airplanes'))
+route.use('/airlines', require('./airlines'))
+route.use('/airport', require('./airport'))
+route.use('/checkinrows', require('./check_in_rows'))
+route.use('/terminals', require('./terminals'))
 
 module.exports = route;
