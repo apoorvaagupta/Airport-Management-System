7167a92061f0193874144cea8ff8256fb6e4e0f3
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-17 01:07:11 +0530

    added the airplane code column in the airplane table
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- db/queries/airplanes.js
+++ db/queries/airplanes.js
@@ -2,6 +2,7 @@
 const createTable = `
   CREATE TABLE IF NOT EXISTS airplanes(
     reg_no      varchar(7)      CONSTRAINT aircraft_registration_number CHECK(reg_no ~ '^[A-Z0-9 -]+$'),
+    icao_code   varchar(6)      CONSTRAINT icao_code CHECK(reg_no ~ '^[A-Z0-9 -]+$'),
     name        varchar(100)    NOT NULL,
     capacity    smallint        NOT NULL CONSTRAINT positive_capacity CHECK(capacity > 0),
     weight      int             NOT NULL CONSTRAINT positive_weight CHECK(weight > 0),
