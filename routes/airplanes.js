a6a2af137d20cbb4934838276c438c24e209f993
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-17 00:06:21 +0530

    added the route for airplanes
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

---
+++ routes/airplanes.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const route = express.Router();
+const db = require('../db/models');
+
+route.post('/add', function (req, res) {
+
+  db.query(`INSERT INTO airplanes values(${req.body.aircraft_registration_number},'${req.body.icao_code}','${req.body.name}',${req.body.capacity},${req.body.weight})`).then((data) => {
+    console.log(data)
+    res.send("Airplane added");
+  }).catch((err) => {
+    console.log(err);
+    res.send(err);
+  })
+})
+
+
+route.get('/viewAll', (req, res) => {
+  db.query("SELECT * FROM airplanes;").then((airplanes) => {
+    console.log(airplanes)
+    res.send(airplanes[0])
+  })
+})
+
+
+module.exports = route;
