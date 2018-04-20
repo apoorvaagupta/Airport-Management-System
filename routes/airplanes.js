e43fc64557b17f150f3ce44a32e77b3b71804d21
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-20 19:55:11 +0530

    added the airplanes routes
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- routes/airplanes.js
+++ routes/airplanes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const route = express.Router();
-const db = require('../db/models');
+const db = require('../db/models').db;
 
 route.post('/add', function (req, res) {
 
