245d6c22984e6074439c759075aab3509884dc2a
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2018-04-20 19:56:11 +0530

    update routes index.js
    Signed-off-by: Apoorvaa Gupta <apoorvaagupta01@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const route = require('express').Router()
 route.use('/airplanes', require('./airplanes'))
 route.use('/airlines', require('./airlines'))
 route.use('/airport', require('./airport'))
-route.use('/checkinrows', require('./check_in_rows'))
+route.use('/check_in_rows.html', require('./check_in_rows'))
 route.use('/terminals', require('./terminals'))
+
+
 
 module.exports = route;
